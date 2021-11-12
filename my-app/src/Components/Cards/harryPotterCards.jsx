8d16816183f1289dc53973e80c8235a84fd0afcd
Roby Morgan <roby.c.morgan@gmail.com>
2021-11-12 15:45:33 +0100

    fixed alignment.

--- my-app/src/Components/Cards/harryPotterCards.jsx
+++ my-app/src/Components/Cards/harryPotterCards.jsx
@@ -1,29 +1,23 @@
+/** @format */
 
+import { Button, Card, Row, Col, Container } from "react-bootstrap";
 
-import { Button, Card, Row, Col, Container} from 'react-bootstrap'
+const HarryPotterCards = ({ displayData }) => (
+  <div className="d-flex">
+    {displayData.map((data) => (
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={data.Poster} />
+        {/* <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body> */}
+      </Card>
+    ))}
+  </div>
+);
 
-const HarryPotterCards = ({displayData}) => (
-
-    <div>
-        {
-
-            displayData.map(data => 
-        
-                    
-                        <Col className=" d-flex col-md-3">
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={data.Poster} />
-                            </Card>
-                        </Col>
-                    
-      
-
-
-            )
-
-        }
-    </div>
-
-)
-
-export default HarryPotterCards
+export default HarryPotterCards;

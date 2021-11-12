0b99ba7a63ad15df244af0ec631aeafef181b959
Roby Morgan <roby.c.morgan@gmail.com>
2021-11-12 12:24:27 +0100

    added bootstrap cards.

---
+++ my-app/src/Components/Cards/cards.jsx
@@ -0,0 +1,34 @@
+import { Component } from 'react'
+import harryPotterCards from '../Cards/harryPotterCards'
+
+class cards extends Component {
+    state = {
+        harryPotter: []
+    }
+
+    componentDidMount () {
+        this.fetchData()
+    }
+    
+    fetchData = async () => {
+        try {
+            const response = await fetch ("http://www.omdbapi.com/?i=tt3896198&apikey=907b4b85&s=harry")
+            if (response.ok) {
+                const data = await response.json()
+                console.log(data)
+                this.setState({harryPotter: data})
+            } else {
+                throw response
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    render() {
+        <div>
+            <harryPotterCards/>
+        </div>
+    }
+}
+
+export default cards

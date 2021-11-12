e00ecb2981dea36439687c31abf8d42bb36d1c87
Roby Morgan <roby.c.morgan@gmail.com>
2021-11-12 13:18:05 +0100

    minor changes.

--- my-app/src/Components/Cards/harryPotterCards.jsx
+++ my-app/src/Components/Cards/harryPotterCards.jsx
@@ -1,19 +1,13 @@
-import Card from 'react-bootstrap/Card'
 
-const HarryPotterCards = ({object}) => (
-    <div>
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            {/* <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body> */}
-        </Card>
-    </div>
+import CardItem from 'react-bootstrap/Card'
+
+const HarryPotterCards = ({displayData}) => (
+    <>
+        {
+            displayData.Search.map(data => 
+                <CardItem key={data.imdbID} info={data}/>)
+        }
+    </>
 )
 
 export default HarryPotterCards

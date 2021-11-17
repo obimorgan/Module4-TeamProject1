2dc069c7a5235a1d5dc6fd9967bd3e4c0a207856
Roby Morgan <roby.c.morgan@gmail.com>
2021-11-17 08:27:03 +0100

    Update HorrorItem.jsx

--- my-app/src/Components/Cards/HorrorItem.jsx
+++ my-app/src/Components/Cards/HorrorItem.jsx
@@ -5,8 +5,8 @@ import { Button, Card, Row, Col, Container } from "react-bootstrap";
 const HorrorItem = ({ horrorData }) => (
   <div className="d-flex">
     {horrorData.slice(0, 6).map((data) => (
-        <Card style={{ width: "250px"}} className="mb-2">
-          <Card.Img className="img-fluid" variant="top" src={data.Poster} />
+        <Card style={{ width: "250px", maxHeight: "150px"}} className="mb-2 mx-1">
+          <Card.Img style={{objectFit: "cover"}} className="img-fluid" variant="top" src={data.Poster} />
         </Card>
     ))}
   </div>
